3055f553c8c880853a099b98a9630a85dba3e61e
hartmut <hartmut@debian>
2022-10-26 08:12:26 +0200

    default for speedbar

--- configuration/gui.el
+++ configuration/gui.el
@@ -2,4 +2,11 @@
 ; A comfortable list of files and open windows
 
 (require 'sr-speedbar)
+
+(setq sr-speedbar-right-side nil
+      sr-speedbar-width 25
+      sr-speedbar-max-width 25)
+
+(setq speedbar-initial-expansion-list-name "quick buffers")
+
 (sr-speedbar-open)
